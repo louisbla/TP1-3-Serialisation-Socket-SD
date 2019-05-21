8a07bf181a3b0b8dd1cfc67fece480a9921fee59
Louis Blasselle <louis.blasselle@gmail.com>
2019-05-21 09:38:22 -0400

    Ajout commentaires + correction block finally

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,6 @@
 package Client;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
@@ -9,31 +10,44 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
+		System.out.println("********* Affichage console du client **********");
+		Socket soc = null;
+		OutputStream os = null;
+		ObjectOutput oos = null;
+		InputStream is = null;
+		ObjectInputStream ios = null;
+		
 		try {
-			//Création d'un nouveau Socket
-			Socket soc = new Socket(InetAddress.getLocalHost(), 8020);
-			//Obtenir un OutputStream à partir du socket
-			OutputStream os = soc.getOutputStream();
-			ObjectOutput oos = new ObjectOutputStream(os);
-			//Ecrire les objets
+			//Création d'un nouveau Socket, et ouverture du flux sortant
+			soc = new Socket(InetAddress.getLocalHost(), 8020);
+			os = soc.getOutputStream();
+			oos = new ObjectOutputStream(os);
+			
+			//Ecrire les objets à envoyer
 			oos.writeObject(Calc.class);
 			System.out.println("Client : La classe Calc a été envoyée au serveur");
 			oos.writeObject("Calc&add&3&5");
 			System.out.println("Client : J'ai demandé au serveur d'utiliser la méthode 'add' avec les paramètres 3 et 5");
-			//Envoyer les objects, et fermer le flux
 			
-			InputStream is = soc.getInputStream();
-			ObjectInputStream ios = new ObjectInputStream(is);
-			
+			//Envoyer les objects
 			oos.flush();
+			
+			//Ouvrir le flux entrant et attendre la réponse
+			is = soc.getInputStream();
+			ios = new ObjectInputStream(is);
 			int result = ios.readInt();
+			
+			//Afficher le résultat et fermer le flux
 			System.out.println("Client : Le serveur a renvoyé le résultat = " + result);
-			oos.close();
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.out.println("Erreur");
 			System.exit(1);
+		}finally {
+			ios.close();
+			oos.close();
 		}
 	}
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,38 +1,33 @@
 package Server;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Date;
 
 public class Server {
 	public static void main(String args[]) {
+		System.out.println("********* Affichage console du serveur **********");
 		ServerSocket ser = null;
 		Socket soc = null;
 		try {
-			//Créer un nouveau ServerSocket
+			//Créer un nouveau Socket
 			ser = new ServerSocket(8020);
 			System.out.println("Server : En attente de contact d'un client");
-			//Obtenir un Socket à partir du ServerSocket
+			
+			//Attendre la connexion d'un client, et ouvrir  un flux entrant
 			soc = ser.accept();
-			//Obtenir un InputStream à partir du socket
 			InputStream is = soc.getInputStream();
-			//Créer un ObjectInputStream à partir du InputStream
 			ObjectInput ios = new ObjectInputStream(is);
-			//Lire les objets
-			Class calc = (Class) ios.readObject();
-			String requete = (String) ios.readObject();
-			String[] substrings = requete.split("&");
+			Class calc = (Class) ios.readObject();		//Reception de la classe
+			String requete = (String) ios.readObject();	//Reception de la requete et paramètres
+			String[] substrings = requete.split("&");	//Séparation des paramètres
+			
 			Object instance = calc.newInstance();
 			
 			Method[] methods = calc.getDeclaredMethods();
@@ -47,25 +42,28 @@ public class Server {
 			
 			method = calc.getMethod("add", String.class, String.class);
 			
+			//Appel de la méthode demandée
 			String[] arguments = {substrings[2],substrings[3]};
 			System.out.println("Server : Appel de la méthode '"+method.getName()+"' avec les paramètres "+ substrings[2] + " et " + substrings[3] );
 			int result = (int)method.invoke(instance, arguments);
+			
 			//Simule un temps de calcul élevé
 			System.out.println("Server : On simule un temps de calcul de 4 secondes...");
 			Thread.sleep(4000);
 			
-			//Afficher les objets
+			//Afficher le résultat à renvoyer
 			System.out.println("Server : resultat = " + result);
 			
+			//Ouverture d'un flux sortant
 			OutputStream os = soc.getOutputStream();
-			//Créer un ObjectInputStream à partir du InputStream
 			ObjectOutput oos = new ObjectOutputStream(os);
+			//Ecriture et envoie du résultat
 			oos.writeInt(result);
 			oos.flush();
 			
-			
 			//Fermer les Stream
 			ios.close();
+			oos.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
